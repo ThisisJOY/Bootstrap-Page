249ef8f6473a4e6b64cd02be8926f0ba0e5d31bd
qiaoqiaoli <joyinbritish@gmail.com>
2016-11-29 13:29:13 +0100

    initial commit

---
+++ pages/explore.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Explore extends Component {
+  render(){
+    return (
+      <div>
+        <h2>Explore IVH2</h2>
+        <div>Some IVH2 page content</div>
+      </div>
+    )
+  }
+}
