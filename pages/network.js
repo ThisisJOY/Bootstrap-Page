03060a77ff1bc88f98d1aaeeb60fd4f7a8710310
qiaoqiaoli <joyinbritish@gmail.com>
2016-11-30 14:23:32 +0100

    static top nav bar and react content

---
+++ pages/network.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Network extends Component {
+  render(){
+    return (
+      <div>
+        <h2>About</h2>
+        <div>Some about page content</div>
+      </div>
+    )
+  }
+}
--- routes.js
+++ routes.js
@@ -6,6 +6,9 @@ import About from './pages/about.js'
 import Explore from './pages/explore.js'
 import Contact from './pages/contact.js'
 import NoMatch from './pages/noMatch.js'
+import Network from './pages/network.js'
+import LANInterface from './pages/LANInterface.js'
+import WLANInterface from './pages/WLANInterface.js'
 
 // Main component
 class App extends Component {
@@ -14,18 +17,29 @@ class App extends Component {
   }
   render(){
     return (
-       <div>
-         <h1>Welcome to In Vehicle Hub 2</h1>
-            <nav>
-               <ul>
-                  <li><Link to="/">Home</Link></li>
-                  <li><Link to="/about">About</Link></li>
-                  <li><Link to="/explore">Explore IVH2</Link></li>
-                  <li><Link to="/contact">Contact</Link></li>
-               </ul>
-            </nav>
-         { this.props.children }
-       </div>
+       //<div>
+         //<h1>Welcome to In Vehicle Hub 2</h1>
+            // <nav>
+               // <ul>
+                  // <li><Link to="/">Home</Link></li>
+                  // <li><Link to="/about">About</Link></li>
+                  // <li><Link to="/explore">Explore IVH2</Link></li>
+                  // <li><Link to="/contact">Contact</Link></li>
+               // </ul>
+            // </nav>
+         // { this.props.children }
+       // </div>
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+          <a class="navbar-brand" href="/">
+            <img class="pull-left mr-15 relative logo__graphic" src="/images/logo.jpg" width="346" height="98"></img>
+            <span class="pull-left gotham-rounded logo__text"></span>
+          </a>
+      </div>
+    </div>
+    { this.props.children }
+    </nav>
     )
   }
 }
@@ -34,6 +48,9 @@ export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home}/>
     <Route path="about" component={About}/>
+    <Route path="network" component={Network}/>
+    <Route path="LANInterface" component={LANInterface}/>
+    <Route path="WLANInterface" component={WLANInterface}/>
     <Route path="explore" component={Explore}/>
     <Route path="contact" component={Contact}/>
     <Route path="*" component={NoMatch}/>
